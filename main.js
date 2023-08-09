9b444a867bc4c8cf5a8c9a7f376913942eeb55c9
Jeyanthi <128176804+Jeyanthi-1234@users.noreply.github.com>
2023-08-09 14:55:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+let id=(id)=>document.getElementById(id);
+
+let classes=(classes)=>document.getElementsByClassName(classes);
+
+let username=id("username"),
+email=id("email"),
+password=id("password"),
+form=id("form"),
+errorMsg=classes("error"),
+successIcon=classes("success-icon"),
+failureIcon=classes("failure-icon");
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault();
+
+  engine(username,0,"username cannot be blank");
+  
+  engine(email,1,"email cannot be blank");
+
+   engine(password,2,"password cannot be blank");
+});
+
+let engine=(id,serial,message)=>{
+  if(id.value.trim()==="")
+{ 
+   errorMsg[serial].innerHTML=message;
+  failureIcon[serial].style.opacity="1";
+  successIcon[serial].style.opacity="0";
+}
+else{
+  errorMsg[serial].innerHTML="";
+  failureIcon[serial].style.opacity="0";
+  successIcon[serial].style.opacity="1";
+}
+};
